d0f51cb913815cdb2bbc2b9c7b210b5df7d7c8d7
sunnextt <sunnext2010@gmail.com>
2021-09-01 23:58:05 +0100

    add search input

---
+++ src/components/SearchInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View,TextInput } from 'react-native'
+
+const SearchInput = () => {
+    return (
+        <View>
+            <TextInput type="text" placeholder="Search"/>
+        </View>
+    )
+}
+
+export default SearchInput
+
+const styles = StyleSheet.create({})
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import SearchInput from '../components/SearchInput'
 
 const SearchScreen = () => {
     return (
         <View>
             <Text style={styles.textScreen}>SearchScreen</Text>
+            <SearchInput/>
         </View>
     )
 }
